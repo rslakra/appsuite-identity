e3c2fdcd35a71bf6ae6c5ca448f0ee819c49d6ac
Rohtash Lakra <rslakra@users.noreply.github.com>
2023-10-28 18:30:11 -0700

    code refactored and moved to respective project

---
+++ src/main/java/com/devamatre/appsuite/identity/IdentityApplication.java
@@ -0,0 +1,9 @@
+package com.devamatre.appsuite.identity;
+
+/**
+ * @author Rohtash Lakra
+ * @created 2/25/22 10:34 AM
+ */
+public class IdentityApplication {
+
+}
---
+++ src/main/java/com/devamatre/appsuite/identity/entity/SecurityGroup.java
@@ -0,0 +1,9 @@
+package com.devamatre.appsuite.identity.entity;
+
+/**
+ * @author Rohtash Lakra
+ * @created 2/25/22 1:30 PM
+ */
+public class SecurityGroup extends Group {
+
+}
